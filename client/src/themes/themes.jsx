822036ad83c4a60af482b1ce16c8c26a66a00e89
rajeshCreate72 <mechdepartment1221@gmail.com>
2024-11-18 23:34:04 +0530

    packages installed

---
+++ client/src/themes/themes.jsx
@@ -0,0 +1,25 @@
+import { createTheme } from '@mui/material/styles';
+
+export const lightTheme = createTheme({
+  palette: {
+    mode: 'light', // Light mode
+    primary: {
+      main: '#1976d2',
+    },
+    secondary: {
+      main: '#ff4081',
+    },
+  },
+});
+
+export const darkTheme = createTheme({
+  palette: {
+    mode: 'dark', // Dark mode
+    primary: {
+      main: '#90caf9',
+    },
+    secondary: {
+      main: '#f48fb1',
+    },
+  },
+});
