5052bb8dd82622adb64759a5142f3245910733b8
rajeshCreate72 <mechdepartment1221@gmail.com>
2024-11-22 02:12:54 +0530

    Revert "Users added, Home page, Login and Register page has been created"
    This reverts commit f142a09db5a58f0de57946940600ac3634342964.

--- client/src/themes/themes.jsx
+++ client/src/themes/themes.jsx
@@ -1,14 +1,13 @@
-import { createTheme } from '@mui/material/styles';
+import { createTheme } from "@mui/material/styles";
 
 export const lightTheme = createTheme({
-<<<<<<< HEAD
     palette: {
         mode: "light", // Light mode
         primary: {
             main: "#1976d2",
         },
         secondary: {
-            main: "#1b4e75",
+            main: "#ff4081",
         },
     },
 });
@@ -17,33 +16,10 @@ export const darkTheme = createTheme({
     palette: {
         mode: "dark", // Dark mode
         primary: {
-            main: "#010114",
+            main: "#90caf9",
         },
         secondary: {
-            main: "#F5F5F5",
+            main: "#f48fb1",
         },
     },
-=======
-  palette: {
-    mode: 'light', // Light mode
-    primary: {
-      main: '#1976d2',
-    },
-    secondary: {
-      main: '#ff4081',
-    },
-  },
-});
-
-export const darkTheme = createTheme({
-  palette: {
-    mode: 'dark', // Dark mode
-    primary: {
-      main: '#90caf9',
-    },
-    secondary: {
-      main: '#f48fb1',
-    },
-  },
->>>>>>> parent of f142a09 (Users added, Home page, Login and Register page has been created)
 });
