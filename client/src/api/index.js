fd37da7138b097728a29afa2d9510efedbce4f93
rajeshCreate72 <mechdepartment1221@gmail.com>
2024-11-22 02:04:55 +0530

    new revised

--- client/src/api/index.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export const axiosInstance = axios.create({
-    baseURL: "http://localhost:8080/",
-    headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${localStorage.getItem("token")}`,
-    },
-});
--- client/src/themes/themes.jsx
+++ client/src/themes/themes.jsx
@@ -1,6 +1,7 @@
-import { createTheme } from "@mui/material/styles";
+import { createTheme } from '@mui/material/styles';
 
 export const lightTheme = createTheme({
+<<<<<<< HEAD
     palette: {
         mode: "light", // Light mode
         primary: {
@@ -22,4 +23,27 @@ export const darkTheme = createTheme({
             main: "#F5F5F5",
         },
     },
+=======
+  palette: {
+    mode: 'light', // Light mode
+    primary: {
+      main: '#1976d2',
+    },
+    secondary: {
+      main: '#ff4081',
+    },
+  },
+});
+
+export const darkTheme = createTheme({
+  palette: {
+    mode: 'dark', // Dark mode
+    primary: {
+      main: '#90caf9',
+    },
+    secondary: {
+      main: '#f48fb1',
+    },
+  },
+>>>>>>> parent of f142a09 (Users added, Home page, Login and Register page has been created)
 });
